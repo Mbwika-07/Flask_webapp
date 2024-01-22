5e862ce1b62fce23a2123c7a3f91ad89b82b4a7b
Mbwika-07 <allanmbwiks@gmail.com>
2024-01-22 20:44:49 +0300

    New changes

---
+++ main.py
@@ -0,0 +1,6 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True) #this ensuers the flask server is started only when this script is run directly. the debug = truethe server auto reloads when code changes and should be turned off when in production
